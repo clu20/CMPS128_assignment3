0974351d48db038d3e26f6b85f75f6d749bd6b2b
Wen Bin Yu <wenbin@Wens-MacBook-Pro.local>
2019-05-20 19:05:56 -0700

    changes to delete function

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ class key_value(Resource):
                 #on key value not found error
                 return make_response(jsonify(doesExist=False, error="Key does not exist", message="Error in GET"), 404)
 
+    #key must also be sent to other replicas' key value store
+    #so just get all the other views and add it 
     def put(self, key):
         if 'FORWARDING_ADDRESS' in os.environ:
             try:
@@ -48,6 +50,7 @@ class key_value(Resource):
                     else:
                     #add new value @ key, key
                         newdict[key] = v
+
                         return make_response(jsonify(message='Added successfully', replaced=False), 201)
                 else:
                     return make_response(jsonify(error="Value is missing",message="Error in PUT"), 400)
@@ -73,7 +76,7 @@ class key_value(Resource):
 class Views(Resource):
     def get(self):
         return make_response(jsonify(message='View retrieved successfully', view = os.environ['VIEW']))
-
+    
     def put(self):
         view_list = os.environ['VIEW'].split(',')
         msg = request.get_json()
@@ -84,11 +87,15 @@ class Views(Resource):
             else:
                 new_view = os.environ['VIEW'] + ',' + socket_add
                 os.environ['VIEW'] = new_view
+                beginning = 'http://'
+                end_point = '/key-value-store-view'
+                replica_url = beginning+socket_add+end_point
+                for key in newdict:
+                	json = requests.get_json()
+                	requests.put(replica_url+'/'+key, json=json)
                 #broadcast the new replica to be in other replica views
                 for view in view_list:
                     if view != os.environ['SOCKET_ADDRESS']:
-                        beginning = 'http://'
-                        end_point = '/key-value-store-view'
                         replica = beginning+view+end_point
                         requests.put(replica, json = {'socket-address': socket_add})
                 return make_response(jsonify(message= 'Replica added successfully to the view'), 200)
@@ -102,15 +109,15 @@ class Views(Resource):
         socket_add = msg.get('socket-address')
         if socket_add in view_list:
             view_list.remove(socket_add)
-            new_view = self.buildView(view_list)
-            # list_length = len(view_list)
-            # x = 0
-            # while x < list_length:
-            #     if(x == list_length - 1):
-            #         new_view += view_list[x]
-            #     else:
-            #         new_view += view_list[x] + ','
-            #     x += 1
+            # new_view = self.buildView(view_list)
+            list_length = len(view_list)
+            x = 0
+            while x < list_length:
+                if(x == list_length - 1):
+                    new_view += view_list[x]
+                else:
+                    new_view += view_list[x] + ','
+                x += 1
             os.environ['VIEW'] = new_view
             for view in view_list:
                 if view != os.environ['SOCKET_ADDRESS']:
